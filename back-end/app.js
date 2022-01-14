1c74ae4d75df048a5a832549a4c36afccef25b40
Maki-Alex <apj.grosjean@gmail.com>
2022-01-14 20:35:10 +0100

    link to MongoDB via npm Mongoose

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://new_user:Project6Piiquante@cluster0.b2khq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
