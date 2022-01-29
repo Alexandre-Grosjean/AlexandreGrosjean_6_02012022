a6085df8e9bba4f11144bf6b7c1a5df61f191e70
Maki-Alex <apj.grosjean@gmail.com>
2022-01-29 14:16:24 +0100

    add multer & like/dislike

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
-const userRoutes = require('./routes/user')
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://new_user:Project6Piiquante@cluster0.b2khq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
 
@@ -18,8 +22,11 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
+});
 
-app.use('/api/auth', userRoutes)
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,11 +1,9 @@
 const bcrypt = require('bcrypt');
-const { JsonWebTokenError } = require('jsonwebtoken');
 const jwt = require('jsonwebtoken')
 
-const User = require('../models/User.js');
+const User = require('../models/User');
 
 exports.signup = (req, res, next) => {
-  console.log(req.body)
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
         const user = new User({
@@ -13,31 +11,31 @@ exports.signup = (req, res, next) => {
             password: hash
         });
         user.save()
-        .then(() =>res.status(201).json ({ message : 'utilisateur crée !'}))
-        .catch(error => res.status(500).json({ error }))
+        .then(() => res.status(201).json ({message: 'Utilisateur crée'}))
+        .catch(error => res.status(400).json ({ error }))
     })
-    .catch(error => res.status(500).json({ error }));
+    .catch(error => res.status(500).json ({ error }));
 };
 
 exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
     .then(user => {
-      if (!user) {
-        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-      }
-      bcrypt.compare(req.body.password, user.password)
+        if (!user) {
+            return res.status(401).json ({ error: 'utilisateur non trouvé !' });
+        }
+        bcrypt.compare(req.body.password, user.password)
         .then(valid => {
-          if (!valid) {
-            return res.status(401).json({ error: 'Mot de passe incorrect !' });
-          }
-          res.status(200).json({
-            userId: user._id,
-            token: jwt.sign(
-                {userId: user._id },
-                'RANDOM_TOKEN_SECRET',
-                { expiresIN: '24h' }
-            )
-          });
+            if (!valid) {
+                return res.status(401).json ({ error: 'mot de passe incorrect !' });
+            }
+            res.status(200).json({
+                userID: user._id,
+                token: jwt.sign(
+                    { userId: user._id },
+                    "RANDOM_TOKEN_SECRET",
+                    { expiresIn: "24h" }
+                )
+            });
         })
         .catch(error => res.status(500).json({ error }));
     })
